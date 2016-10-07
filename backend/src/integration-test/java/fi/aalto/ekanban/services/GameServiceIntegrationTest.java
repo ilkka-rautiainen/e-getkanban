34ea5f7b5057b5418aa988470c85ea1e3eef67a7
anttiahonen <antti.ahonen@aalto.fi>
2016-10-07 09:27:49 +0300

    Refactored a spring integration base class for integration tests to use

--- backend/src/integration-test/java/fi/aalto/ekanban/services/GameServiceIntegrationTest.java
+++ backend/src/integration-test/java/fi/aalto/ekanban/services/GameServiceIntegrationTest.java
@@ -5,26 +5,18 @@ import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import de.bechte.junit.runners.context.HierarchicalContextRunner;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestContextManager;
 
+import fi.aalto.ekanban.SpringIntegrationTest;
 import fi.aalto.ekanban.models.games.Game;
 
 @RunWith(HierarchicalContextRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class GameServiceIntegrationTest {
+public class GameServiceIntegrationTest extends SpringIntegrationTest {
 
     @Autowired
     private GameService gameService;
-
-    @Before
-    public void setUp() throws Exception {
-        new TestContextManager(getClass()).prepareTestInstance(this);
-    }
 
     public class startGame {
 
