05dba5570fc02efa1eefb62964d34bb5031815f2
Ilkka Rautiainen <ilkka.rautiainen@vincit.fi>
2016-12-19 10:47:56 +0200

    Backend: Add validation for wip-limits

--- backend/src/main/java/fi/aalto/ekanban/models/AdjustWipLimitsAction.java
+++ backend/src/main/java/fi/aalto/ekanban/models/AdjustWipLimitsAction.java
@@ -2,9 +2,12 @@ package fi.aalto.ekanban.models;
 
 import java.util.Map;
 
+import fi.aalto.ekanban.validators.CheckWipLimits;
+
 public class AdjustWipLimitsAction {
 
     // Phase id -> new wip-limit
+    @CheckWipLimits
     private Map<String, Integer> phaseWipLimits;
 
     public Map<String, Integer> getPhaseWipLimits() {
---
+++ backend/src/main/java/fi/aalto/ekanban/validators/CheckWipLimits.java
@@ -0,0 +1,21 @@
+package fi.aalto.ekanban.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = CheckWipLimitsValidator.class)
+public @interface CheckWipLimits {
+    String message() default "Invalid wip limits given";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
