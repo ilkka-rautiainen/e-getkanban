59e64d1e9d4217691a64c88985bbef0526a819ac
Ilkka Rautiainen <ilkka.rautiainen@vincit.fi>
2017-01-04 15:52:19 +0200

    Backend: Add leadTimeInDays to Card

--- backend/src/main/java/fi/aalto/ekanban/models/db/games/Card.java
+++ backend/src/main/java/fi/aalto/ekanban/models/db/games/Card.java
@@ -15,6 +15,9 @@ public class Card extends BaseCard {
     private Integer dayDeployed;
 
     @Field
+    private Integer leadTimeInDays;
+
+    @Field
     private Integer subscribers;
 
     @Field
@@ -34,6 +37,14 @@ public class Card extends BaseCard {
 
     public void setDayDeployed(Integer dayDeployed) {
         this.dayDeployed = dayDeployed;
+    }
+
+    public Integer getLeadTimeInDays() {
+        return leadTimeInDays;
+    }
+
+    public void setLeadTimeInDays(Integer leadTimeInDays) {
+        this.leadTimeInDays = leadTimeInDays;
     }
 
     public Integer getSubscribers() {
@@ -74,6 +85,7 @@ public class Card extends BaseCard {
         if (outcome != null ? !outcome.equals(card.outcome) : card.outcome != null) return false;
         if (dayStarted != null ? !dayStarted.equals(card.dayStarted) : card.dayStarted != null) return false;
         if (dayDeployed != null ? !dayDeployed.equals(card.dayDeployed) : card.dayDeployed != null) return false;
+        if (leadTimeInDays != null ? !leadTimeInDays.equals(card.leadTimeInDays) : card.leadTimeInDays != null) return false;
         if (orderNumber != null ? !orderNumber.equals(card.orderNumber) : card.orderNumber != null) return false;
         return subscribers != null ? subscribers.equals(card.subscribers) : card.subscribers == null;
     }
@@ -83,6 +95,7 @@ public class Card extends BaseCard {
         int result = super.hashCode();
         result = 31 * result + (dayStarted != null ? dayStarted.hashCode() : 0);
         result = 31 * result + (dayDeployed != null ? dayDeployed.hashCode() : 0);
+        result = 31 * result + (leadTimeInDays != null ? leadTimeInDays.hashCode() : 0);
         result = 31 * result + (subscribers != null ? subscribers.hashCode() : 0);
         result = 31 * result + (orderNumber != null ? orderNumber.hashCode() : 0);
         return result;
