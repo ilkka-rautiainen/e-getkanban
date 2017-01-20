0126b00885c3842e7cdb00d8ced015aed7d2bbbf
anttiahonen <antti.ahonen@aalto.fi>
2017-01-20 12:53:53 +0200

    Frontend / CFD config name fixing and separated different configs to separate files

---
+++ frontend/src/components/CFD/CFDGraphConfigTemplate.js
@@ -0,0 +1,22 @@
+const cfdGraphConfigTemplate = {
+  "balloon":{
+    "drop":true,
+    "adjustBorderColor":false,
+    "color":"#ffffff",
+  },
+  "balloonColor": "",
+  "bullet": "round",
+  "bulletBorderAlpha": 1,
+  "bulletColor": "#FFFFFF",
+  "bulletSize": 5,
+  "hideBulletsCount": 50,
+  "lineThickness": 2,
+  "useLineColorForBulletBorder": true,
+  "fillAlphas": 0.0,
+  "lineAlpha": 0.8,
+  "title": "",
+  "valueField": "",
+  "lineColor": ""
+};
+
+export default cfdGraphConfigTemplate;
