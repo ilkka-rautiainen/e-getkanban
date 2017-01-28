561797c377a9f8c562be09386689b115cb3fb46d
Ilkka Rautiainen <ilkka.rautiainen@vincit.fi>
2017-01-28 18:35:37 +0200

    Backend: Add equals tests to models missing them

--- backend/src/main/java/fi/aalto/ekanban/models/AdjustWipLimitsAction.java
+++ backend/src/main/java/fi/aalto/ekanban/models/AdjustWipLimitsAction.java
@@ -18,4 +18,23 @@ public class AdjustWipLimitsAction {
         this.phaseWipLimits = phaseWipLimits;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        if (!(o instanceof AdjustWipLimitsAction)) return false;
+
+        AdjustWipLimitsAction action = (AdjustWipLimitsAction) o;
+
+        return phaseWipLimits != null ? phaseWipLimits.equals(action.phaseWipLimits) : action.phaseWipLimits == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = phaseWipLimits != null ? phaseWipLimits.hashCode() : 0;
+        return result;
+    }
+
 }
--- backend/src/main/java/fi/aalto/ekanban/models/DrawFromBacklogAction.java
+++ backend/src/main/java/fi/aalto/ekanban/models/DrawFromBacklogAction.java
@@ -23,4 +23,25 @@ public class DrawFromBacklogAction {
         this.indexToPlaceCardAt = indexToPlaceCardAt;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        if (!(o instanceof DrawFromBacklogAction)) return false;
+
+        DrawFromBacklogAction action = (DrawFromBacklogAction) o;
+
+        if (deckType != null ? !deckType.equals(action.deckType) : action.deckType != null) return false;
+        return indexToPlaceCardAt != null ? indexToPlaceCardAt.equals(action.indexToPlaceCardAt) : action.indexToPlaceCardAt == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = deckType != null ? deckType.hashCode() : 0;
+        result = 31 * result + (indexToPlaceCardAt != null ? indexToPlaceCardAt.hashCode() : 0);
+        return result;
+    }
+
 }
