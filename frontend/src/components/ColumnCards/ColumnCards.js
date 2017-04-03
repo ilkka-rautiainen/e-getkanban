110c92e62ffde16612c5a0ed148666f85e3ceb2e
anttiahonen <antti.ahonen@aalto.fi>
2017-04-03 11:16:01 +0300

    Frontend / Use react-motion for move card action

--- frontend/src/components/ColumnCards/ColumnCards.js
+++ frontend/src/components/ColumnCards/ColumnCards.js
@@ -1,14 +1,68 @@
 import React, { PropTypes } from 'react';
+import {TransitionMotion, spring, presets} from 'react-motion';
 import Card from '../Card/Card';
 
-const ColumnCards = ({ columnCardIds }) => {
-  return <div className="column-cards">
-    {columnCardIds.map(cardId => <Card key={cardId} id={cardId} />)}
-  </div>
-};
+const ColumnCards = React.createClass({
+  getDefaultStyles() {
+    const styles = this.props.columnCardIds.map((cardId) => {
+      return {
+        key: cardId,
+        style: { opacity: 0.0 }
+      }
+    });
+    return styles;
+  },
+  getStyles() {
+    const styles = this.props.columnCardIds.map((cardId) => {
+      return {
+        key: cardId,
+        style: {
+          opacity: spring(1, {stiffness: 220, damping: 42})
+        }
+      }
+    });
+    return styles;
+  },
+
+  willEnter() {
+    return { opacity: 0.0 }
+  },
+  willLeave() {
+    return {
+      opacity: spring(0, presets.gentle)
+    };
+  },
+
+  render() {
+    if (this.props.columnCardIds) {
+      return (
+        <TransitionMotion
+          defaultStyles={this.getDefaultStyles()}
+          styles={this.getStyles()}
+          willEnter={this.willEnter}
+          willLeave={this.willLeave}
+        >
+          {styles =>
+            <div className="column-cards">
+              {styles.map((card) => {
+                return (
+                  <Card key={card.key} id={card.key} style={card.style} />
+                )
+              })}
+            </div>
+          }
+        </TransitionMotion>
+      )
+    } else {
+      return null;
+    }
+
+  },
+
+});
 
 ColumnCards.propTypes = {
-  columnCardsIds: PropTypes.arrayOf(PropTypes.string.isRequired)
+  columnCardIds: PropTypes.arrayOf(PropTypes.string.isRequired)
 };
 
 export default ColumnCards;
